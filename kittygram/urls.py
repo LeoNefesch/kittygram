5b1ccbc1d57c5e748828972f7386260faa95bc8e
leonid.negaschev <leonid.negaschev@gmail.com>
2023-02-08 10:14:01 +0500

    Add combined generics

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,9 @@
 from django.urls import include, path
 
-from cats.views import APICat, APICatDetail
+from cats.views import CatDetail, CatList
 
 urlpatterns = [
-   path('cats/', APICat.as_view(), name='cat_list'),
-   path('cats/<int:id>/', APICatDetail.as_view(), name='cat_detail'),
+   path('cats/', CatList.as_view()),
+   path('cats/<int:pk>/', CatDetail.as_view()),
 ]
 
