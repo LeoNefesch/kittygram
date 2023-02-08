d90c28eb2a4b5bbd86931031640697d64ab946a7
leonid.negaschev <leonid.negaschev@gmail.com>
2023-02-08 08:31:45 +0500

    Add view functions

--- cats/views.py
+++ cats/views.py
@@ -9,7 +9,7 @@ from .serializers import CatSerializer
 @api_view(['GET', 'POST'])
 def cat_list(request):
     if request.method == 'POST':
-        serializer = CatSerializer(data=request.data)
+        serializer = CatSerializer(data=request.data, many=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -17,3 +17,27 @@ def cat_list(request):
     cats = Cat.objects.all()
     serializer = CatSerializer(cats, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def cat_detail(request, id):
+    cat = Cat.objects.get(id=id)
+    if request.method == 'PUT' or request.method == 'PATCH':
+        serializer = CatSerializer(cat, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        cat.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    serializer = CatSerializer(cat)
+    return Response(serializer.data)
+
+
+@api_view(['GET', 'POST'])
+def hello(request):
+    # В ответ на POST-запрос возвращаем JSON с данными из запроса.
+    if request.method == 'POST':
+        return Response({'message': 'Получены данные', 'data': request.data})
+    return Response({'message': 'Это был GET-запрос!'})
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 
-from cats.views import cat_list
+from cats.views import cat_detail, cat_list, hello
 
 urlpatterns = [
-   path('cats/', cat_list),
+   path('cats/', cat_list, name='cat_list'),
+   path('cats/<int:id>/', cat_detail, name='cat_detail'),
+   path('hello/', hello, name='hello'),
 ]
-
 
